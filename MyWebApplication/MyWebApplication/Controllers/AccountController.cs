c4550a50104e9f1e6720c89ab87584c39cb6243a
MarkAndrewManoy <markandrewm@apc.edu.ph>
2023-09-15 17:29:34 +0800

    For Class Discussion of Retrieval and Update

---
+++ MyWebApplication/MyWebApplication/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebApplication.Models.EntityManager;
+using MyWebApplication.Models.ViewModel;
+
+namespace MyWebApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            UserManager um = new UserManager();
+            UsersModel user = um.GetAllUsers();
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager um = new UserManager();
+                if (!um.IsLoginNameExist(user.LoginName))
+                {
+                    um.AddUserAccount(user);
+                    // FormsAuthentication.SetAuthCookie(user.FirstName, false);
+                    return RedirectToAction("", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Login Name already taken.");
+            }
+            return View();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update([FromBody] UserModel userData)
+        {
+            UserManager um = new UserManager();
+            if (um.IsLoginNameExist(userData.LoginName))
+            {
+                um.UpdateUserAccount(userData);
+                return RedirectToAction("Index"); // Redirect to a relevant action after successful update.
+            }
+            // Handle the case when the login name doesn't exist, e.g., return a relevant error view.
+            return RedirectToAction("LoginNameNotFound");
+        }
+
+    }
+}
